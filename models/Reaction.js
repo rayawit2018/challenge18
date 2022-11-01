d500198b92234a3d7d6fc44ce34a0d01dddcbf13
rayawit2018 <39783901+rayawit2018@users.noreply.github.com>
2022-11-01 09:15:57 -0400

    Add files via upload

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const { Schema, Types } = require('mongoose');
+
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+            
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+)
+
+
+module.exports = ReactionSchema;
